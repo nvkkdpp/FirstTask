942fc6bcef07618d8e2d0b661249b12673523c65
nvkkdpp <niggamemes@ya.ru>
2022-05-26 01:56:49 +0900

    FirstCommit

---
+++ 111/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _111
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите ваше имя");
+
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Привет, " + name);
+
+            Console.WriteLine("Нажмите <enter> для выхода . . .");
+
+            Console.Read();
+        }
+    }
+}
